b024c66eb115518d0f1aec4ed38541b17bf20d3e
子非 <1553060424@qq.com>
2021-04-25 14:37:03 +0800

    Process_CreateWorkbook 优化代码结构,更新注释

--- RPAAction/Excel CSO/Process_CreateWorkbook.cs
+++ RPAAction/Excel CSO/Process_CreateWorkbook.cs
@@ -5,6 +5,7 @@ namespace RPAAction.Excel_CSO
 {
     /// <summary>
     /// 进程-创建工作簿
+    /// 如果创建的文件已经存在则抛出异常
     /// </summary>
     class Process_CreateWorkbook : ExcelAction
     {
@@ -22,21 +23,12 @@ namespace RPAAction.Excel_CSO
             {
                 throw new ActionException("请指定参数\"wbPath\"");
             }
-
-            //检查目录以及路径
-            string dir = Path.GetDirectoryName(wbPath);
-            if (Directory.Exists(dir))
+            if (File.Exists(wbPath))
             {
-                if (File.Exists(wbPath))
-                {
-                    throw new ActionException(string.Format("文件({0})已经存在", wbPath));
-                }
-            }
-            else
-            {
-                Directory.CreateDirectory(dir);
+                throw new ActionException(string.Format("文件({0})已经存在", wbPath));
             }
 
+            Directory.CreateDirectory(Path.GetDirectoryName(wbPath));
             wb = app.Workbooks.Add();
             ws.SaveAs(wbPath);
         }
