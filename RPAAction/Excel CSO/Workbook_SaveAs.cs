9c51a55a4fe098c9c4a49a7b4d9a8b58f1147c0a
子非 <1553060424@qq.com>
2021-04-26 22:44:50 +0800

    工作簿-另存为

---
+++ RPAAction/Excel CSO/Workbook_SaveAs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPAAction.Base;
+
+namespace RPAAction.Excel_CSO
+{
+    /// <summary>
+    /// 工作簿-另存为
+    /// 如果 newWbPath 存在文件将抛出异常
+    /// </summary>
+    class Workbook_SaveAs : ExcelAction
+    {
+        public Workbook_SaveAs(string wbPath = null, string newWbPath = null)
+            : base(wbPath)
+        {
+            this.newWbPath = Path.GetFullPath(newWbPath);
+            Run();
+        }
+
+        protected override void action()
+        {
+            base.action();
+
+            if (File.Exists(newWbPath))
+            {
+                throw new ActionException(string.Format("文件({0})已经存在", newWbPath));
+            }
+
+            Directory.CreateDirectory(Path.GetDirectoryName(newWbPath));
+            wb.SaveAs(newWbPath, getXlFileFormatByWbPath(newWbPath));
+        }
+
+        private string newWbPath = null;
+    }
+}
