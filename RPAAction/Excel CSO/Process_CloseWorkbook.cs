ebdf89214f7913a773e261b09cff7626cf19ed1a
子非 <1553060424@qq.com>
2021-05-24 23:29:13 +0800

    Process_CloseWorkbook

---
+++ RPAAction/Excel CSO/Process_CloseWorkbook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPAAction.Excel_CSO
+{
+    /// <summary>
+    /// 进程-关闭工作簿
+    /// </summary>
+    class Process_CloseWorkbook : ExcelAction
+    {
+        public Process_CloseWorkbook(string wbPath = null, bool isSave  = false)
+            : base(wbPath)
+        {
+            this.isSave = isSave;
+            Run();
+        }
+
+        protected override void action()
+        {
+            if (CheckString(wbPath))
+            {
+                wb = AttachWorkbook(wbPath);
+                if (wb != null)
+                {
+                    wb.Close(isSave);
+                }
+            }
+            else
+            {
+                base.action();
+                wb.Close(isSave);
+            }
+        }
+
+        private readonly bool isSave;
+    }
+}
